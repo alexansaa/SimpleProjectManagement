bd50e45bd2c78dfe31a4bebb437df84afa7a1a4e
alexansaa <alexan.saa@gmail.com>
2024-01-15 19:03:11 -0500

    Objects Implementation

---
+++ src/main/java/com/example/application/data/Project.java
@@ -0,0 +1,149 @@
+package com.example.application.data;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+public class Project implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String projectName;
+    private Date creationDate;
+    private Date dueDate;
+    private String description;
+    private int numberOfTasks;
+    private List<User> assignedUsers;
+    private User creatorOwner;
+    private List<Task> taskList;
+
+    // Constructor
+    public Project(String projectName, Date creationDate, Date dueDate, String description,
+                   int numberOfTasks, List<User> assignedUsers, User creatorOwner, List<Task> taskList) {
+        this.projectName = projectName;
+        this.creationDate = creationDate;
+        this.dueDate = dueDate;
+        this.description = description;
+        this.numberOfTasks = numberOfTasks;
+        this.assignedUsers = assignedUsers;
+        this.creatorOwner = creatorOwner;
+        this.taskList = taskList;
+    }
+
+    // Getters
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getNumberOfTasks() {
+        return numberOfTasks;
+    }
+
+    public List<User> getAssignedUsers() {
+        return assignedUsers;
+    }
+
+    public User getCreatorOwner() {
+        return creatorOwner;
+    }
+
+    public List<Task> getTaskList() {
+        return taskList;
+    }
+
+    // Setters
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setNumberOfTasks(int numberOfTasks) {
+        this.numberOfTasks = numberOfTasks;
+    }
+
+    public void setAssignedUsers(List<User> assignedUsers) {
+        this.assignedUsers = assignedUsers;
+    }
+
+    public User getAssignedUserByIndex(int index) {
+        return this.assignedUsers.get(index);
+    }
+
+    public void addAssignedUser(User user) {
+        this.assignedUsers.add(user);
+    }
+
+    public void updateAssignedUser(User user, User newUser) {
+        int index = this.assignedUsers.indexOf(user);
+        this.assignedUsers.set(index, newUser);
+    }
+
+    public void deleteAssignedUser(User user) {
+        int index = this.assignedUsers.indexOf(user);
+        this.assignedUsers.remove(index);
+    }
+
+    public void setCreatorOwner(User creatorOwner) {
+        this.creatorOwner = creatorOwner;
+    }
+
+    public void setTaskList(List<Task> taskList) {
+        this.taskList = taskList;
+    }
+
+    public Task getTaskByIndex(int index) {
+        return this.taskList.get(index);
+    }
+
+    public void addTask(Task task) {
+        this.taskList.add(task);
+    }
+
+    public void updateTask(Task task, Task newTask) {
+        int index = this.taskList.indexOf(task);
+        this.taskList.set(index, newTask);
+    }
+
+    public void removeTask(Task task) {
+        int index = this.taskList.indexOf(task);
+        this.taskList.remove(index);
+    }
+
+    // toString method for easy printing
+    @Override
+    public String toString() {
+        return "Project{" +
+                "projectName='" + projectName + '\'' +
+                ", creationDate=" + creationDate +
+                ", dueDate=" + dueDate +
+                ", description='" + description + '\'' +
+                ", numberOfTasks=" + numberOfTasks +
+                ", assignedUsers=" + assignedUsers +
+                ", creatorOwner='" + creatorOwner + '\'' +
+                // ", taskList=" + taskList +
+                '}';
+    }
+}
+
---
+++ src/main/java/com/example/application/data/Task.java
@@ -0,0 +1,108 @@
+package com.example.application.data;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+public class Task implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String taskName;
+    private String description;
+    private Date creationDate;
+    private List<User> assignedUsers;
+    private String taskStatus;
+    private List<Comment> comments;
+
+    // Constructor
+    public Task(String taskName, String description, Date creationDate,
+                List<User> assignedUsers, String taskStatus, List<Comment> comments) {
+        this.taskName = taskName;
+        this.description = description;
+        this.creationDate = creationDate;
+        this.assignedUsers = assignedUsers;
+        this.taskStatus = taskStatus;
+        this.comments = comments;
+    }
+
+    // Getters
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public List<User> getAssignedUsers() {
+        return assignedUsers;
+    }
+
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    // Setters
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public void setAssignedUsers(List<User> assignedUsers) {
+        this.assignedUsers = assignedUsers;
+    }
+
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public Comment getCommentByIndex(int index) {
+        return this.comments.get(index);
+    }
+
+    public void addComment(Comment comment) {
+        this.comments.add(comment);
+    }
+
+    public void updateComment(Comment comment, Comment newComment) {
+        int index =  this.comments.indexOf(comment);
+        this.comments.set(index, newComment);
+    }
+
+    public void deleteComment(Comment comment) {
+        int index = this.comments.indexOf(comment);
+        this.comments.remove(index);
+    }
+
+    // toString method for easy printing
+    @Override
+    public String toString() {
+        return "Task{" +
+                "taskName='" + taskName + '\'' +
+                ", description='" + description + '\'' +
+                ", creationDate=" + creationDate +
+                ", assignedUsers=" + assignedUsers +
+                ", taskStatus='" + taskStatus + '\'' +
+                // ", comments=" + comments +
+                '}';
+    }
+}
